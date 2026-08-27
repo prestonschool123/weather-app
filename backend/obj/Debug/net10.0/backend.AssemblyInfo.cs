9f34518a8d346f06201085d1bf747e3836107dc8
prestonschool123 <153012541+prestonschool123@users.noreply.github.com>
2026-08-27 10:33:43 -0700

    Latest changes to the app

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d03987f2144a5242f60141df66b9be1fa56e910b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67da9475521d979af8c935f316f7f7fc430291fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
